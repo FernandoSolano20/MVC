3f0eea741c839b7657e831c2efdbb01f9534b296
Andres Bonilla <abonillaa@ucenfotec.ac.cr>
2020-02-11 14:01:24 -0600

    Se agrega la primera version del patron MVC

---
+++ MVC_UI/Controllers/ClientController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_UI.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult CreateClient()
+        {
+            return View();
+        }
+
+        public ActionResult ReadClient()
+        {
+            return View();
+        }
+    }
+}
