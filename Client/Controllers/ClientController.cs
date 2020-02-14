36ecd87905ae4ea09a93c22d51f39e303669eec9
Fernando Solano <fesolano@outcoding.com>
2020-02-14 02:15:04 -0600

    Upload API changes

---
+++ Client/Controllers/ClientController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Client.Models;
+using CoreApiService;
+using POJOS;
+
+namespace Client.Controllers
+{
+    public class ClientController : ApiController
+    {
+        ApiResponse apiResponse = new ApiResponse();
+
+        public IHttpActionResult Get()
+        {
+
+            apiResponse = new ApiResponse();
+            var mng = new ClientManager();
+            apiResponse.Data = mng.RetrieveAll();
+
+            return Ok(apiResponse);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var mng = new ClientManager();
+            var client = new ClientType
+            {
+                Id = id
+            };
+
+            client = mng.RetrieveById(client);
+            apiResponse = new ApiResponse();
+            apiResponse.Data = client;
+            return Ok(apiResponse);
+        }
+
+        public IHttpActionResult Post(ClientType client)
+        {
+            var mng = new ClientManager();
+            mng.Create(client);
+
+            apiResponse = new ApiResponse();
+            apiResponse.Message = "Action was executed.";
+
+            return Ok(apiResponse);
+        }
+
+        public IHttpActionResult Put(ClientType client)
+        {
+            var mng = new ClientManager();
+            mng.Update(client);
+
+            apiResponse = new ApiResponse();
+            apiResponse.Message = "Action was executed.";
+
+            return Ok(apiResponse);
+        }
+
+        // DELETE ==
+        public IHttpActionResult Delete(ClientType client)
+        {
+            var mng = new ClientManager();
+            mng.Delete(client);
+
+            apiResponse = new ApiResponse();
+            apiResponse.Message = "Action was executed.";
+
+            return Ok(apiResponse);
+        }
+    }
+}
---
+++ CoreApiService/ClientManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Crud.Client;
+using POJOS;
+
+namespace CoreApiService
+{
+    public class ClientManager : BaseManager
+    {
+        private ClientCrudFactory clientCrudFactory;
+
+        public ClientManager()
+        {
+            clientCrudFactory = new ClientCrudFactory();
+        }
+
+        public void Create(ClientType client)
+        {
+            clientCrudFactory.Create(client);
+        }
+
+        public List<ClientType> RetrieveAll()
+        {
+            return clientCrudFactory.RetrieveAll<ClientType>();
+        }
+
+        public ClientType RetrieveById(ClientType client)
+        {
+               return clientCrudFactory.Retrieve<ClientType>(client);
+        }
+
+        public void Update(ClientType client)
+        {
+            clientCrudFactory.Update(client);
+        }
+
+        public void Delete(ClientType client)
+        {
+            clientCrudFactory.Delete(client);
+        }
+    }
+}
